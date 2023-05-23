c4b4350ec01f77fc9dcbc0f7a2e6f6c3a0c2bf4e
Thiago Sabara <thi.sil.sab@gmail.com>
2023-05-22 22:41:46 -0300

    feat: add version on api responses
    Now all api responses will have the actual version.
    
    Closes #0001

--- api/api_core/ping_pong.py
+++ api/api_core/ping_pong.py
@@ -2,18 +2,18 @@ from ..setup import __version__
 
 
 async def route_post(word):
-    return {"message": "PONG", "method": "POST", 'word': word}
+    return {"message": "PONG", "method": "POST", 'word': word, "vs": __version__}
 
 
 async def route_get(word=None):
     dict_1 = {"message": "PONG", "method": "GET", "vs": __version__}
-    return dict_1 if not word else {"message": "PONG", "method": "GET", "word": word}
+    return dict_1 if not word else {"message": "PONG", "method": "GET", "word": word, "vs": __version__}
 
 
 async def route_put():
-    return {"message": "PONG", "method": "PUT"}
+    return {"message": "PONG", "method": "PUT", "vs": __version__}
 
 
 async def route_delete():
-    return {"message": "PONG", "method": "DELETE"}
+    return {"message": "PONG", "method": "DELETE", "vs": __version__}
 
