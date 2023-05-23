2894de4f13b82929ef89f1009c96336777d70172
Thiago Sabara <thi.sil.sab@gmail.com>
2023-05-22 22:29:17 -0300

    fix $file: change ping_pong
    Modification of file(4).
    
    BREAKING CHANGE: Apenas completmentando o texto deste commit.

--- api/api_core/ping_pong.py
+++ api/api_core/ping_pong.py
@@ -1,10 +1,12 @@
+from ..setup import __version__
+
 
 async def route_post(word):
     return {"message": "PONG", "method": "POST", 'word': word}
 
 
 async def route_get(word=None):
-    dict_1 = {"message": "PONG", "method": "GET"}
+    dict_1 = {"message": "PONG", "method": "GET", "vs": __version__}
     return dict_1 if not word else {"message": "PONG", "method": "GET", "word": word}
 
 
