8cf03b086d1836a1caafe273fff64c9e68fe4a66
Thiago Sabara <thi.sil.sab@gmail.com>
2023-06-15 09:45:09 -0300

    chore: commit to start CD v5a
    only for CD
    
    Tasks:
     - [ ] cover the project on TDD

--- api/api_core/root.py
+++ api/api_core/root.py
@@ -2,6 +2,7 @@ from fastapi import Response, status
 
 
 def global_route(response: Response):
+    """Just for commit"""
     response.status_code = status.HTTP_403_FORBIDDEN
     return {
         "message": "invalid_resource",
