2b48cca76034de130022c3607d7fc3f4cfd5f800
Thiago Sabara <thi.sil.sab@gmail.com>
2023-06-22 22:19:06 -0300

    chore: update gitignore
    only for CD, retrying
    
    Tasks:
     - [ ] cover the project on TDD

--- api/api_core/root.py
+++ api/api_core/root.py
@@ -2,10 +2,6 @@ from fastapi import Response, status
 
 
 def global_route(response: Response):
-    """Just for commit5"""
+    """Rota de resposta padrão da aplicação."""
     response.status_code = status.HTTP_403_FORBIDDEN
-    return {
-        "message": "invalid_resource",
-        "status": "error",
-        "status_code": 200
-    }
+    return {"message": "invalid_resource", "status": "error", "status_code": 200}
