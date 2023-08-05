0a2a46a9ff99f18a64f57a66c113aa285977d8c2
Thiago Sabara <thi.sil.sab@gmail.com>
2023-08-04 23:44:06 -0300

    fix: docker exec
    ajustes de execução no docker
    
    Tasks:
     - [ ] cover the project on TDD

--- manage.py
+++ manage.py
@@ -1,28 +1,45 @@
+#!/usr/bin/python
 
-import logging
 import os
 import argparse
 import uvicorn
 
+from api.application import __version__
 from dotenv import load_dotenv
 
 load_dotenv()
+
+CLI_VERSION = __version__
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("runserver", help="inicia a aplicação", type=str)
+    parser.add_argument("--runserver", help="inicia a aplicação", required=False, action="store_true")
     parser.add_argument("--port", help="porta de acesso", type=int, default=8000)
+    parser.add_argument("--stage", help="stage de desenvolvimento", type=str, default="dev")
+    parser.add_argument("--version", help="versão da aplicação", required=False, action="store_true")
 
     args = parser.parse_args()
 
-    if args.runserver == "runserver":
-        logging.info(f'API on air! vs: {os.environ.get("API_VERSION")}')
+    if args.version:
+        print("API: ", CLI_VERSION)
+        exit(0)
+
+    if args.runserver and args.stage == "dev":
+        print(f'API on air! vs: {os.environ.get("API_VERSION")}')
         uvicorn.run(
             app="api.application:app",
             host="127.0.0.1",
             reload=True,
+            port=args.port
+        )
+
+    if args.runserver and args.stage == "prod":
+        print(f'API on air! vs: {os.environ.get("API_VERSION")}')
+        uvicorn.run(
+            app="api.application:app",
+            host="0.0.0.0",
             port=args.port
         )
 
